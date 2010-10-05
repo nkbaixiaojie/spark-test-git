d3f67e8590356d11f0734de1f3f19d66f8e5523a
spark.bai <nkbaixiaojie@gmail.com>
2010-10-06 00:19:33 +0800

    first commit, create the new project

---
+++ src/com/spark/git_test/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.spark.git_test;
+
+/**
+ * 
+ * @author spark.bai(nkbaixiaojie@gmail.com)
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+
+}
